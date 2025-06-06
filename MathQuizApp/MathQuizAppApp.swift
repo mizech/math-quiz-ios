44df8cb72d7d7c4e2c062e5a33b26e581642e8a4
mizech <michael.zech@hotmail.de>
2025-06-06 11:19:26 +0200

    Initial Commit

---
+++ MathQuizApp/MathQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathQuizAppApp.swift
+//  MathQuizApp
+//
+//  Created by Michael on 06.06.25.
+//
+
+import SwiftUI
+
+@main
+struct MathQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
