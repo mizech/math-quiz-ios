60cf0c83968a2c44aa4d726e28c71065e28f5061
mizech <michael.zech@hotmail.de>
2025-06-21 13:20:44 +0200

    Improved: ResultView

--- MathQuizApp/Views/ResultView.swift
+++ MathQuizApp/Views/ResultView.swift
@@ -6,22 +6,38 @@ struct ResultView: View {
 	@Binding var isSheetShown: Bool
 	
     var body: some View {
-		VStack {
-			Text("Game over").font(.title)
-			Text("You have accomplished \(mainVM.points) points.")
-			Button {
-				mainVM.setInitValues(hasGameStarted: false)
-				isSheetShown = false
-			} label: {
-				Text("Restart")
-					.frame(height: 40)
-					.frame(maxWidth: .infinity)
-					.background(.blue)
-					.foregroundStyle(.white)
-					.clipShape(RoundedRectangle(cornerRadius: 12))
+		NavigationStack {
+			VStack {
+				Text("Game over").font(.largeTitle)
+					.padding(.bottom)
+				Text("You have accomplished \(mainVM.points) points.")
+					.font(.title2)
+				Spacer()
+				Button {
+					mainVM.setInitValues(hasGameStarted: false)
+					isSheetShown = false
+				} label: {
+					Text("Restart")
+						.frame(height: 40)
+						.frame(maxWidth: .infinity)
+						.bold()
+						.background(.blue)
+						.foregroundStyle(.white)
+						.clipShape(RoundedRectangle(cornerRadius: 12))
+				}
+				Spacer()
+			}.padding()
+			.interactiveDismissDisabled(true)
+			.toolbar {
+				ToolbarItem(placement: .topBarTrailing) {
+					Button {
+						isSheetShown = false
+					} label: {
+						Image(systemName: "multiply.circle.fill")
+					}
+				}
 			}
-		}.padding()
-		.interactiveDismissDisabled(true)
+		}
     }
 }
 
